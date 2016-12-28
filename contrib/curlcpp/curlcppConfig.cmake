4d8eeebedc9546db548271481ca7486e1400cecb
Oleg Chernovskiy <adonai@xaker.ru>
2016-12-28 09:36:32 +0300

    Replace dir with upstream

--- contrib/curlcpp/curlcppConfig.cmake
+++
@@ -1,5 +0,0 @@
-# curlcppConfig.cmake - Configuration file for external projects.
-
-set(CURLCPP_INCLUDE_DIRS "/home/adonai/Work/C++/MailFuse/contrib/curlcpp/include")
-set(CURLCPP_LIBRARY_DIRS "/home/adonai/Work/C++/MailFuse/contrib/curlcpp/src")
-set(CURLCPP_LIBRARIES "$<TARGET_FILE:>")
--- contrib/curlcpp/src/CMakeFiles/curlcpp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for curlcpp.
-# This may be replaced when dependencies are built.
